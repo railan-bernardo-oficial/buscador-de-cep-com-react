a1b966b83221d8233ab4ebd8494c628392e8da6b
Railan Bernardo <railan.bernardo@leadforce.com.br>
2023-03-14 07:49:06 -0300

    Buscador de cep com react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+
+import api from './services/api';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState({});
+
+  async function handleSearch() {
+
+    if (input === '') {
+      alert('Digite um CEP para buscar');
+      return;
+    }
+
+    try {
+
+      const response = await api.get(`${input}/json`);
+      setResult(response.data);
+      setInput('');
+
+    } catch (error) {
+      alert('Erro ao buscar CEP');
+      setInput('');
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>Buscador de CEP</h1>
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu CEP..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className='submit'
+          onClick={handleSearch}>
+          <FiSearch size={26} color="#FFF" />
+        </button>
+      </div>
+
+      {Object.keys(result).length > 0 && (
+ <div className="containerResult">
+ <h2 className='titleResult'>CEP: {result.cep}</h2>
+ <span>Cidade: {result.localidade} - {result.uf}</span>
+ <span>Rua: {result.logradouro}</span>
+ <span>Bairro: {result.bairro}</span>
+ <span>Complemento: {result.complemento} </span>
+</div>
+)};
+     
     </div>
   );
 }
